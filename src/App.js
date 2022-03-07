56f8e352ccd3caace1e834fe8e9bdc90d38af216
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-03-07 16:56:27 -0300

    Requisito 1 OK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+import './index.css';
 
 class App extends React.Component {
   render() {
-    return (<p>Iniciando PR</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile/edit" render={ () => <ProfileEdit /> } />
+          <Route path="/profile" render={ () => <Profile /> } />
+          <Route path="/favorites" render={ () => <Favorites /> } />
+          <Route path="/album/:id" render={ () => <Album /> } />
+          <Route path="/search" render={ () => <Search /> } />
+          <Route exact path="/" render={ () => <Login /> } />
+          <Route path="*" render={ () => <NotFound /> } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/Album.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="page-album">
+          Album
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Album;
