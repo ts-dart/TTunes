ba410c1cde332d4cda36cdf9bfdcf0ef07685bcf
thiago <thiagoedusan5.11@outlook.com>
2023-03-02 21:22:53 -0300

    construindo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,20 @@ export default class Footer extends React.Component {
       <footer>
         <p>Made by Thiago</p>
         <div>
-          <a href='https://github.com/ts-dart' target='_blank' rel="noreferrer"><img src='' alt=''/></a>
-          <a href='https://www.linkedin.com/in/thiago-henrique-da-silva-souza-634162127/' target='_blank' rel="noreferrer"><img src='' alt=''/></a>
+          <a href='https://github.com/ts-dart' target='_blank' rel="noreferrer">
+            <img 
+              id='imgGithubLogo'
+              src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-github-96.png' 
+              alt='GitHub logo'
+            />
+            </a>
+          <a href='https://www.linkedin.com/in/thiago-henrique-da-silva-souza-634162127/' target='_blank' rel="noreferrer">
+            <img 
+              id='imgLinkedinLogo'
+              src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-linkedin-50.png' 
+              alt='Linkedin logo'
+            />
+          </a>
         </div>
       </footer>
     );
