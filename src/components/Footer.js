76d4a366457dfcfd629172c77859462b64f8a491
thiago <thiagoedusan5.11@outlook.com>
2023-03-02 21:53:31 -0300

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,24 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>Made by Thiago</p>
-        <div>
-          <a href='https://github.com/ts-dart' target='_blank' rel="noreferrer">
-            <img 
-              id='imgGithubLogo'
-              src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-github-96.png' 
-              alt='GitHub logo'
-            />
+        <div id='footerContent'>
+          <p>Made by Thiago</p>
+          <div>
+            <a href='https://github.com/ts-dart' target='_blank' rel="noreferrer">
+              <img 
+                id='imgGithubLogo'
+                src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-github-96.png' 
+                alt='GitHub logo'
+              />
+              </a>
+            <a href='https://www.linkedin.com/in/thiago-henrique-da-silva-souza-634162127/' target='_blank' rel="noreferrer">
+              <img 
+                id='imgLinkedinLogo'
+                src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-linkedin-50.png' 
+                alt='Linkedin logo'
+              />
             </a>
-          <a href='https://www.linkedin.com/in/thiago-henrique-da-silva-souza-634162127/' target='_blank' rel="noreferrer">
-            <img 
-              id='imgLinkedinLogo'
-              src='https://raw.githubusercontent.com/ts-dart/TTunes/main/public/images/icons8-linkedin-50.png' 
-              alt='Linkedin logo'
-            />
-          </a>
+          </div>
         </div>
       </footer>
     );
